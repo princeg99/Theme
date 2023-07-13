ea4069e945d937df4faecec5d7c37c0add260f37
princeg99 <princeg@unityinfoway.com>
2023-07-13 19:17:55 +0530

    Casual add

--- js/main.js
+++ js/main.js
@@ -18,3 +18,23 @@ $('#home1-testimonial').owlCarousel({
         }
     }
 });
+$('#testimonial-page').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        769:{
+            items: 2,
+            nav:false
+        },
+        1201:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+});
